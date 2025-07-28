58ce7e457dcd3e5e20273df27326a03ade36c622
hamexhanif <hamexhanif@yahoo.com>
2025-07-28 13:46:43 +0200

    initilized Spring Boot backend with required dependencies

---
+++ backend/src/main/java/io/werescuecats/backend/WeRescueCatsBackendApplication.java
@@ -0,0 +1,13 @@
+package io.werescuecats.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeRescueCatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeRescueCatsBackendApplication.class, args);
+	}
+
+}
